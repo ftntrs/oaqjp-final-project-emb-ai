e1bcea22d0bc5b2aef6fe208aaec6d8d38b4eae1
Your Name <you@example.com>
2025-07-26 07:42:28 -0400

    after pylint

--- server.py
+++ server.py
@@ -1,3 +1,6 @@
+"""
+    SERVER.PY
+"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,6 +8,8 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def emotion_detector_function():
+    ''' This function calls the application
+    '''
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
 
@@ -21,7 +26,10 @@ def emotion_detector_function():
 
 @app.route("/")
 def render_index_page():
+    ''' This is the function to render the html interface
+    '''
     return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", port = 5000)
+    

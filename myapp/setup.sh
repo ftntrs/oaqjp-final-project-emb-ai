7323f0f67e33af5c8fee00481abf12b287eb52a9
WilliamS <73660888+cnwyshi@users.noreply.github.com>
2025-07-26 06:11:58 -0700

    try flask route

--- myapp/setup.sh
+++ myapp/setup.sh
@@ -3,6 +3,7 @@ virtualenv my_env # create a virtual environment my_env
 source my_env/bin/activate # activate my_env
 
 python3 -m pip install transformers==4.30.2 torch
-
+python3 -m pip install flask
+python3 -m pip install flask_cors
 
 

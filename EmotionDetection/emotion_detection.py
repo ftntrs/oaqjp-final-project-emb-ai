ad1bddd9dedd965688ecb5a11a29128ca699eb90
Your Name <you@example.com>
2025-07-26 07:07:23 -0400

    3a_output_formatting

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,19 +1,15 @@
-import json
 import requests
 
-'''
-URL: 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-Headers: {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-Input json: { "raw_document": { "text": text_to_analyse } }
-'''
-def emotion_detector(text_to_analyze):
-    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'  # 情感分析服务的 URL
-    myobj = { "raw_document": { "text": text_to_analyze } }  # 创建一个字典，包含要分析的文本
-    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}  # 设置 API 请求所需的头信息
-    response = requests.post(url, json = myobj, headers=header)  # 发送 POST 请求到 API，包含文本和头信息
-    formatted_response = json.loads(response.text)
-    emotion_scores = formatted_response['emotionPredictions'][0]['emotion']
-    dominant_emotion = max(emotion_scores, key=emotion_scores.get)
-    response = {**emotion_scores, 'dominant_emotion': dominant_emotion}
-    # formatted_response = json.dumps(formatted_response, indent=4)
-    return response
+import json
+
+def emotion_detector(text_to_analyse):
+
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    myobj = {"raw_document": { "text": text_to_analyse } }
+    response = requests.post(url, json = myobj, headers = headers)
+    res = json.loads(response.text)
+    formatted_response = res['emotionPredictions'][0]['emotion']
+    dominant_emotion = max(formatted_response, key = lambda x: formatted_response[x])
+    formatted_response['dominant_emotion'] = dominant_emotion
+    return formatted_response 

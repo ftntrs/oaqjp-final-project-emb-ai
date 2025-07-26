b0f19d80f647c04beb66c222594b73f50543655e
Your Name <you@example.com>
2025-07-26 07:27:37 -0400

    6b_deployment_test

--- server.py
+++ server.py
@@ -3,42 +3,20 @@ from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detector")
 
-# curl localhost:5000/emotionDetector?statement='I love my life'
-# curl localhost:5000/emotionDetector?statement="I love my life"
-# curl 'localhost:5000/emotionDetector?statement=I love my life'
-# curl 'http://localhost:5000/emotionDetector?statement=I%20love%20my%20life'
 @app.route("/emotionDetector")
-def handle_emotion():
-    statement = request.args.get('statement')
-    if not statement or statement.strip() == "":
-        return {
-            'anger': None,
-            'disgust': None,
-            'fear': None,
-            'joy': None,
-            'sadness': None,
-            'dominant_emotion': None,
-            'status_code': 400
-        }
-    response = emotion_detector(statement)
-    # return "给定的文本已被识别为 {}，得分为 {}.".format(label.split('_')[1], score)
-    return (
-        f"For the given statement, the system response is "
-        f"'anger': {response['anger']}, "
-        f"'disgust': {response['disgust']}, "
-        f"'fear': {response['fear']}, "
-        f"'joy': {response['joy']} and 'sadness': {response['sadness']}. "
-        f"The dominant emotion is {response['dominant_emotion']}.\n", 200
-    )
+def emotion_detector_function():
+    text_to_analyze = request.args.get('textToAnalyze')
+    response = emotion_detector(text_to_analyze)
+    response_text = f"For the given statement, the system response is 'anger': \
+                    {response['anger']}, 'disgust': {response['disgust']}, \
+                    'fear': {response['fear']}, 'joy': {response['joy']}, \
+                    'sadness': {response['sadness']}. The dominant emotion is \
+                    {response['dominant_emotion']}."
+    return response_text
+
+@app.route("/")
+def render_index_page():
+    return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
-
-'''
-theia@theiadocker-cnshirui:/home/project/final_project$ curl 'http://localhost:5000/emotionDetector?statement=I%20love%20my%20life'
-<!doctype html>
-<html lang=en>
-<title>500 Internal Server Error</title>
-<h1>Internal Server Error</h1>
-<p>The server encountered an internal error and was unable to complete your request. Either the server is overloaded or there is an error in the application.</p>
-'''
+    app.run(host = "0.0.0.0", port = 5000)
